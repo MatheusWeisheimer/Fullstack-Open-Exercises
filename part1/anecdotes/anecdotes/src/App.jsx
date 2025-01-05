fabf4d890762f76b2bdef10fed1e9ba7e49cee24
Matheus <matheusweish@gmail.com>
2025-01-05 18:55:47 -0300

    1.14 done

--- part1/anecdotes/anecdotes/src/App.jsx
+++ part1/anecdotes/anecdotes/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+const Header = ({text}) => <h1>{text}</h1>
+
 const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
@@ -18,20 +20,45 @@ const App = () => {
   const [votes, setVotes] = useState({})
   
   const handleNext = () => setSelected(Math.floor(Math.random() * anecdotes.length))
-
+  
   const handleVote = () => {
     setVotes({
       ...votes,
       [selected]: votes[selected] ? votes[selected] + 1 : 1
     })
   }
+
+  const getMostVoted = () => {
+    let mostVotes = 0
+    let mostVoted
+
+    for (let option in votes) {
+      if (votes[option] > mostVotes) {
+        mostVotes = votes[option]
+        mostVoted = option
+      }
+    }
+    
+    return mostVoted
+  }
   
+  const mostVoted = getMostVoted()
+
   return (
     <div>
+      <Header text="Anecdote of the day"/>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected] ? votes[selected] : 0} votes</p>
       <Button text={"vote"} onClick={handleVote}/>
       <Button text={"next anecdote"} onClick={handleNext}/>
+      {
+        mostVoted &&
+        <>
+          <Header text="Anecdote with most votes"/>
+          <p>{anecdotes[mostVoted]}</p>
+          <p>has {votes[mostVoted]} votes</p>
+        </> 
+      }
     </div>
   )
 }
