ff0daecf5c30e42df4ad8ea662938c0029c772ff
Matheus <matheusweish@gmail.com>
2025-01-15 22:26:04 -0300

    4.2 done

--- part4/blog-list/blogList/index.js
+++ part4/blog-list/blogList/index.js
@@ -1,47 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-    .then(() => console.log('connected to MongoDB'))
-    .catch(error => console.log('error connecting to MongoDB:', error.message))
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch(error => next(error))
-})
-
-app.post('/api/blogs', (request, response, next) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
-})
-
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
