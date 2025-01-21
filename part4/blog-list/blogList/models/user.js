1f897d8e4f4f342be6cac68ca2aabe2df80b27f2
Matheus <matheusweish@gmail.com>
2025-01-20 21:40:23 -0300

    4.15 done

---
+++ part4/blog-list/blogList/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: String,
+    passowordHash: String,
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'blog',
+        },
+    ],
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passowordHash
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
