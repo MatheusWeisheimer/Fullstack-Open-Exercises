16922dd3c110b12537bf0e67f13849eb108796d3
Matheus <matheusweish@gmail.com>
2025-01-28 20:03:45 -0300

    4.19 done

--- part4/blog-list/blogList/controllers/blogs.js
+++ part4/blog-list/blogList/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {  
+    const authorization = request.get('authorization')
+
+    if (authorization && authorization.startsWith('Bearer ')) {
+            return authorization.replace('Bearer ', '')  
+    }  
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogList = await Blog
@@ -11,12 +21,20 @@ blogsRouter.get('/', async (request, response) => {
 })
   
 blogsRouter.post('/', async (request, response) => {
-    const user = await User.findOne({})
+    const body = request.body
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)  
+    if (!decodedToken.id) {    
+        return response.status(401).json({ error: 'token invalid' })  
+    }  
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
-        ...request.body,
+        title: body.title,
+        author: user.name,
+        url: body.url,
+        likes: body.likes || 0,
         user: user.id,
-        likes: request.body.likes || 0
     })
 
     const savedBlog = await blog.save()
