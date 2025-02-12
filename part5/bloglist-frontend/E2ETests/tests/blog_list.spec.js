8a1a8742ac7019ecbc31b210b893d28e7238d2eb
Matheus <matheusweish@gmail.com>
2025-02-12 18:53:45 -0300

    5.18 done

--- part5/bloglist-frontend/E2ETests/tests/blog_list.spec.js
+++ part5/bloglist-frontend/E2ETests/tests/blog_list.spec.js
@@ -1,12 +1,45 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/tests/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Mr Tester',
+        username: 'tester',
+        password: 'testing'
+      }
+    })
+
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
     const locator = await page.getByText('log in to application')
-    await expect(locator).toBeVisible 
+    await expect(locator).toBeVisible() 
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.locator('div').filter({ hasText: /^username$/ })
+        .getByRole('textbox').fill('tester')
+      await page.locator('div').filter({ hasText: /^password$/ })
+        .getByRole('textbox').fill('testing')
+      await page.getByRole('button').click()
+
+      const locator = await page.getByText('tester logged in')
+      await expect(locator).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.locator('div').filter({ hasText: /^username$/ })
+        .getByRole('textbox').fill('tester')
+      await page.locator('div').filter({ hasText: /^password$/ })
+        .getByRole('textbox').fill('wrong')
+      await page.getByRole('button').click()
+
+      const locator = await page.getByText('wrong username or password')
+      await expect(locator).toBeVisible()
+    })
   })
 })
